b7ab9bf8fdf5b2f465b8897d1f337a43b1b80b70
Colin Liu <y.aotsuki@gmail.com>
2022-01-07 22:13:26 +0800

    fix kde built but not run

--- scripts/merge_files.sh
+++ scripts/merge_files.sh
@@ -3,6 +3,10 @@ chmod 600 files/etc/dropbear/*
 eval `cat .config | grep \" | head -n 10`
 . files/etc/opkg/distfeeds.conf | tee files/etc/opkg/distfeeds.conf
 
+mkdir -p /opt/kodexplorer
+pushd /opt/kodexplorer && wget https://static.kodcloud.com/update/download/kodbox.1.26.zip && unzip kodbox.1.26.zip
+popd
+
 echo 'iptables -t nat -A PREROUTING -p udp --dport 53 -j REDIRECT --to-ports 53' >> files/etc/firewall.user
 echo 'iptables -t nat -A PREROUTING -p tcp --dport 53 -j REDIRECT --to-ports 53' >> files/etc/firewall.user
 echo '[ -n "$(command -v ip6tables)" ] && ip6tables -t nat -A PREROUTING -p udp --dport 53 -j REDIRECT --to-ports 53' >> files/etc/firewall.user
--- scripts/merge_packages.sh
+++ scripts/merge_packages.sh
@@ -32,7 +32,7 @@ merge_feed nas_luci "https://github.com/linkease/nas-packages-luci;main"
 merge_package https://github.com/klever1988/helloworld/branches/tmp/luci-app-ssr-plus
 merge_package https://github.com/klever1988/helloworld/branches/tmp/mosdns
 #merge_package https://github.com/klever1988/openwrt-mos/trunk/luci-app-mosdns
-merge_package https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-kodexplorer
+merge_package https://github.com/aotsukiqx/nanopi-openwrt/trunk/package/aotsuki/luci-app-kodexplorer
 merge_package https://github.com/project-lede/luci-app-godproxy
 merge_package https://github.com/sundaqiang/openwrt-packages/trunk/luci-app-wolplus
 merge_package https://github.com/coolsnowwolf/lede/trunk/package/lean/automount
