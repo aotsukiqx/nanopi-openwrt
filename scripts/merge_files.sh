59cc04645198ab1322aa9470b31682c877d4db68
Colin Liu <y.aotsuki@gmail.com>
2022-01-09 20:36:06 +0800

    Update merge_files.sh

--- scripts/merge_files.sh
+++ scripts/merge_files.sh
@@ -1,8 +1,10 @@
+echo "Merging files..\n"
+echo "pwd:$(pwd)"
 mkdir -p $GITHUB_WORKSPACE/files/opt/kodexplorer
-pushd $GITHUB_WORKSPACE/files/opt/kodexplorer && wget https://static.kodcloud.com/update/download/kodbox.1.26.zip && unzip kodbox.1.26.zip
-rm -r kodbox.1.26.zip && popd
+pushd $GITHUB_WORKSPACE/files/opt/kodexplorer; wget https://static.kodcloud.com/update/download/kodbox.1.26.zip; unzip kodbox.1.26.zip; rm -r kodbox.1.26.zip; popd
 chmod 600 $GITHUB_WORKSPACE/files/opt/kodexplorer 
 
+echo "Moving files"
 mv $GITHUB_WORKSPACE/files ./
 chmod 600 files/etc/dropbear/*
 eval `cat .config | grep \" | head -n 10`
