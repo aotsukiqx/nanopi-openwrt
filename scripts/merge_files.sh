f1cf48bd3e219e06a24ffc5e71b634ab078d9fc0
Colin Liu <y.aotsuki@gmail.com>
2022-01-09 13:10:15 +0800

    move kodbox files first step of merge files
    modify opwnwrt link

--- scripts/merge_files.sh
+++ scripts/merge_files.sh
@@ -1,11 +1,11 @@
+pushd $GITHUB_WORKSPACE/files/opt/kodexplorer && wget https://static.kodcloud.com/update/download/kodbox.1.26.zip && unzip kodbox.1.26.zip
+rm -r kodbox.1.26.zip && popd
+chmod 600 $GITHUB_WORKSPACE/files/opt/kodexplorer 
+
 mv $GITHUB_WORKSPACE/files ./
 chmod 600 files/etc/dropbear/*
 eval `cat .config | grep \" | head -n 10`
 . files/etc/opkg/distfeeds.conf | tee files/etc/opkg/distfeeds.conf
-
-mkdir -p files/opt/kodexplorer
-pushd files/opt/kodexplorer && wget https://static.kodcloud.com/update/download/kodbox.1.26.zip && unzip kodbox.1.26.zip
-popd
 
 echo 'iptables -t nat -A PREROUTING -p udp --dport 53 -j REDIRECT --to-ports 53' >> files/etc/firewall.user
 echo 'iptables -t nat -A PREROUTING -p tcp --dport 53 -j REDIRECT --to-ports 53' >> files/etc/firewall.user
