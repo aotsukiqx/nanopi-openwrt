e04a824a6b8dbd127f872951618861ae74af04ac
Colin Liu <y.aotsuki@gmail.com>
2022-01-15 16:02:21 +0800

    Update patches.sh

--- scripts/patches.sh
+++ scripts/patches.sh
@@ -29,7 +29,7 @@ if [ $BRANCH == 'master' ]; then
   git checkout target/linux/x86
   git revert --no-commit -X theirs 91eed5d9fb74e6c740291362ba12e11a2222a9fd
   
-  sed -i 's/\#[]CONFIG_BLK_CGROUP_IOCOST.*/CONFIG_BLK_CGROUP_IOCOST=y/g' target/linux/generic/config-5.10
+  sed -i "s/#[]CONFIG_BLK_CGROUP_IOCOST.*/CONFIG_BLK_CGROUP_IOCOST=y/g" target/linux/generic/config-5.10
   echo 'CONFIG_BLK_CGROUP_IOCOST=y' >> target/linux/rockchip/armv8/config-5.10
   echo 'CONFIG_BLK_CGROUP_IOCOST=y' >> target/linux/x86/generic/config-5.10
   echo '# CONFIG_KCSAN is not set' >> target/linux/x86/config-5.10
