33f577eb739efe38edf9e67c677302e954e72a06
Colin Liu <y.aotsuki@gmail.com>
2022-02-20 16:22:14 +0800

    Update patches.sh

--- scripts/patches.sh
+++ scripts/patches.sh
@@ -39,24 +39,24 @@ if [ $BRANCH=='master' ]; then
   find package/ -follow -type d -path '*/po/zh-cn' | xargs dirname | xargs -n1 -i sh -c "rm -f {}/zh_Hans; ln -sf zh-cn {}/zh_Hans"
 
   # remove non-exist package from x86 profile
-  sed -i 's/kmod-i40evf//' target/linux/x86/Makefile
+  #sed -i 's/kmod-i40evf//' target/linux/x86/Makefile
 
-  # enable r2s oled plugin by default
-  sed -i "s/enable '0'/enable '1'/" `find package/ -follow -type f -path '*/luci-app-oled/root/etc/config/oled'`
+  if [[ $DEVICE -ne 'r4s' || $DEVICE -ne 'r2s' || $DEVICE -ne 'r1s' || $DEVICE -ne 'r2c' ]]; then
+    # enable r2s oled plugin by default
+    sed -i "s/enable '0'/enable '1'/" `find package/ -follow -type f -path '*/luci-app-oled/root/etc/config/oled'`
 
-  # swap the network adapter driver to r8168 to gain better performance for r4s
-  sed -i 's/r8169/r8168/' target/linux/rockchip/image/armv8.mk
+    # swap the network adapter driver to r8168 to gain better performance for r4s
+    sed -i 's/r8169/r8168/' target/linux/rockchip/image/armv8.mk
 
-  # change the voltage value for over-clock stablization
-  sed -i 's/1400000/1450000/' target/linux/rockchip/patches-5.10/991-arm64-dts-rockchip-add-more-cpu-operating-points-for.patch
-  config_file_cpufreq=`find package/ -follow -type f -path '*/luci-app-cpufreq/root/etc/config/cpufreq'`
-  truncate -s-1 $config_file_cpufreq
-  echo -e "\toption governor0 'schedutil'" >> $config_file_cpufreq
-  echo -e "\toption minfreq0 '816000'" >> $config_file_cpufreq
-  echo -e "\toption maxfreq0 '1512000'\n" >> $config_file_cpufreq
+    # change the voltage value for over-clock stablization
+    sed -i 's/1400000/1450000/' target/linux/rockchip/patches-5.10/991-arm64-dts-rockchip-add-more-cpu-operating-points-for.patch
+    config_file_cpufreq=`find package/ -follow -type f -path '*/luci-app-cpufreq/root/etc/config/cpufreq'`
+    truncate -s-1 $config_file_cpufreq
+    echo -e "\toption governor0 'schedutil'" >> $config_file_cpufreq
+    echo -e "\toption minfreq0 '816000'" >> $config_file_cpufreq
+    echo -e "\toption maxfreq0 '1512000'\n" >> $config_file_cpufreq
 
-  if [[ $DEVICE=='r4s' || $DEVICE=='r2s' || $DEVICE=='r1s' || $DEVICE=='r2c' ]]; then
-  git clean -f -d target/linux/rockchip
+    git clean -f -d target/linux/rockchip
     # enable the gpu for device 'r2s'|'r2c'|'r4s'|'r1p'
     wget https://github.com/coolsnowwolf/lede/raw/757e42d70727fe6b937bb31794a9ad4f5ce98081/target/linux/rockchip/config-default -NP target/linux/rockchip/
     wget https://github.com/coolsnowwolf/lede/commit/f341ef96fe4b509a728ba1281281da96bac23673.patch
