9def15390cafa94cabeb27d27c5584c33e29f4b5
aotsukiqx <y.aotsuki@gmail.com>
2022-05-18 12:15:47 +0800

    Update syncgw.sh

--- files/root/syncgw.sh
+++ files/root/syncgw.sh
@@ -6,11 +6,11 @@
 # 3. 重启新GW并修改LAN IP和配置WAN口拨号
 # 4. 替换GW并部署Heimdall
 
-scp -r /etc/dropbear root@192.168.2.9:/etc/dropbear
+scp -r /etc/dropbear/ root@192.168.2.9:/etc/dropbear/
 scp /etc/config/passwall root@192.168.2.9:/etc/config/
 scp /etc/config/passwall_server root@192.168.2.9:/etc/config/
 scp /etc/config/passwall_show root@192.168.2.9:/etc/config/
-scp -r /usr/share/passwall/rules root@192.168.2.9:/usr/share/passwall/rules
+scp -r /usr/share/passwall/rules root@192.168.2.9:/usr/share/passwall
 scp /etc/apcupsd/apcupsd.conf root@192.168.2.9:/etc/apcupsd/
 scp /etc/config/uhttpd root@192.168.2.9:/etc/config/
 scp /etc/config/autoreboot root@192.168.2.9:/etc/config/
@@ -20,6 +20,6 @@ scp /etc/config/zerotier root@192.168.2.9:/etc/config/
 scp /etc/config/bandwidthd root@192.168.2.9:/etc/config/
 scp -r /etc/config/zero root@192.168.2.9:/etc/config/
 scp /etc/v2ray/config.json root@192.168.2.9:/etc/v2ray/config.json
-scp -r /root/heimdall root@192.168.2.9:/root/heimdall
-scp -r /root/v2s_cert root@192.168.2.9:/root/v2s_cert
-scp -r /root/docker-baseimage-ubuntu-bionic root@192.168.2.9:/root/docker-baseimage-ubuntu-bionic
+scp -r /root/heimdall root@192.168.2.9:/root/
+scp -r /root/v2s_cert root@192.168.2.9:/root/
+scp -r /root/docker-baseimage-ubuntu-bionic root@192.168.2.9:/root/
